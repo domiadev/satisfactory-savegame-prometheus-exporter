27770598388e2afc7bef4d3205780287ce4ebd94
Joakim Hedlund <contact@joakimhedlund.com>
2024-10-25 23:45:49 +0200

    fix: include production from water extractors

--- src/metricGroups/resources.ts
+++ src/metricGroups/resources.ts
@@ -24,12 +24,12 @@ const metrics = new MetricGroup('satisfactory_savegame_resources')
 
 /* eslint-disable no-useless-return */
 export const parser = (object: SaveComponent | SaveEntity, lookups: Lookups): void => {
+  // Don't include manually paused buildings
+  const isStandby = (object?.properties?.mIsProductionPaused as BoolProperty)?.value
+  if (isStandby) return
+
   if (object.properties?.mCurrentRecipe) {
     if (!isObjectProperty(object.properties.mCurrentRecipe)) return
-
-    // Don't include paused production
-    const isStandby = (object.properties?.mIsProductionPaused as BoolProperty)?.value
-    if (isStandby) return
 
     // Clock speed affects the recipe rates
     const clockSpeed =
@@ -50,8 +50,28 @@ export const parser = (object: SaveComponent | SaveEntity, lookups: Lookups): vo
     return
   }
 
-  // TODO: include resource extraction as production
+  // #region Resource extraction
   if (object.properties?.mExtractableResource) {
+    // Waterpumps refer to undocumented bodies of water as their resource nodes, so we treat them differently.
+    if (object.typePath.startsWith('/Game/FactoryGame/Buildable/Factory/WaterPump')) {
+      const item = staticData.items.Desc_Water_C
+      const miner = pathToMiner(object.typePath)
+      if (!miner) throw new Error('Miner not found: ' + object.typePath)
+      const extractionPerSecond = miner.itemsPerCycle / miner.extractCycleTime
+      const clockSpeed =
+        (object.properties?.mCurrentPotential as FloatProperty)?.value ??
+        (object.properties?.mPendingPotential as FloatProperty)?.value ??
+        1
+      metrics.getGauge('production_per_second').inc({ item: item.name }, extractionPerSecond * clockSpeed)
+      return
+    }
+
+    // TODO: geothermal seems tricky because the resourcenode produces "Desc_Geyser_C" which... is odd
+    if (object.typePath.startsWith('/Game/FactoryGame/Buildable/Factory/GeneratorGeoThermal/Build_GeneratorGeoThermal')) return
+    // TODO: fracking is also different, but in its own ✨special✨ way
+    if (object.typePath.startsWith('/Game/FactoryGame/Buildable/Factory/FrackingSmasher')) return
+    if (object.typePath.startsWith('/Game/FactoryGame/Buildable/Factory/FrackingExtractor')) return
+
     const resource = pathToResourceNode((object.properties.mExtractableResource as ObjectProperty).value.pathName)
     if (!resource) throw new Error('Resource not found: ' + (object.properties.mExtractableResource as ObjectProperty).value.pathName)
 
@@ -68,9 +88,9 @@ export const parser = (object: SaveComponent | SaveEntity, lookups: Lookups): vo
       1
     metrics.getGauge('production_per_second').inc({ item: item.name }, extractionPerSecond * resource.purity * clockSpeed)
   }
-  // TODO: no sure if frackers or oilpumps have mExtractableResource
-  // TODO: Waterpumps arent in staticData.miners
-  // if (object.typePath.startsWith('/Game/FactoryGame/Buildable/Factory/WaterPump')) {}
+  // #endregion
+
+  // TODO: water consumption in e.g. generators isnt included yet. Should look at waterToPowerRatio on the generators staticData
 }
 /* eslint-enable no-useless-return */
 
