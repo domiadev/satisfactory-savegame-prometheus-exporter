0c015d22fc2983a38c9e68c7e4f45ba73b6cb75c
Joakim Hedlund <contact@joakimhedlund.com>
2024-10-27 12:53:46 +0100

    feat: `satisfactory_savegame_buildings_lightweight_total` metric
    Because lightweight buildings are static, high cardinality, and not indicative of factory size or performance I opted to place them in a metric easily distinguishable from `satisfactory_savegame_buildings_total`.

--- src/metricGroups/buildings.ts
+++ src/metricGroups/buildings.ts
@@ -2,6 +2,7 @@ import { pathToBuilding } from '../staticData/staticData'
 import { type Lookups } from '../types/lookups.type'
 import { MetricGroup } from './_MetricGroup'
 import {
+  isBuildableSubsystemSpecialProperties,
   type SaveComponent,
   type SaveEntity,
 } from '@etothepii/satisfactory-file-parser'
@@ -12,11 +13,28 @@ const metrics = new MetricGroup('satisfactory_savegame_buildings')
     'Total amount of buildings',
     ['building'],
   )
+  .addGauge(
+    'lightweight_total',
+    'Total amount of lightweight buildings',
+    ['building'],
+  )
 
 export const parser = (object: SaveComponent | SaveEntity, lookups: Lookups): void => {
   const building = pathToBuilding(object.typePath)
   if (building) {
     metrics.getGauge('total').inc({ building: building.name })
+  }
+
+  // "Lightweight" buildings with no dynamic properties such as walls and foundations
+  if (object.typePath === '/Script/FactoryGame.FGLightweightBuildableSubsystem') {
+    if (isBuildableSubsystemSpecialProperties(object.specialProperties)) {
+      for (const buildable of object.specialProperties.buildables) {
+        // Because there are so many variations of walls and foundations, we group them.
+        // E.g. '/Game/FactoryGame/Buildable/Building/Wall/FicsitWallSet/Build_Wall_Orange_8x1.Build_Wall_Orange_8x1_C'
+        const category = buildable.typePath.split('/')[5]
+        metrics.getGauge('lightweight_total').inc({ building: category }, buildable.instances.length)
+      }
+    }
   }
 }
 
